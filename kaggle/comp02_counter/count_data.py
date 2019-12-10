52e182dd7a01d80ede221b43010844ad5b7d33b5
Alexander Kalinovsky (armsi) <gakarak@gmail.com>
2019-12-10 20:03:43 +0300

    validation dataset

--- kaggle/comp02_counter/count_data.py
+++ kaggle/comp02_counter/count_data.py
@@ -5,6 +5,8 @@ __author__ = 'ar'
 import os
 import cv2
 import time
+import glob
+import pickle as pkl
 import skimage.io as io
 import numpy as np
 import pandas as pd
@@ -52,7 +54,7 @@ def get_random_crop_img(sample: dict, rnd_angles: tuple, rnd_scales: tuple, crop
                               dsize=(crop_size, crop_size),
                               borderMode=cv2.BORDER_REFLECT,
                               flags=cv2.INTER_LINEAR)
-    msk_crop = cv2.warpAffine(img, rnd_mat,
+    msk_crop = cv2.warpAffine(msk, rnd_mat,
                               dsize=(crop_size, crop_size),
                               borderMode=cv2.BORDER_REFLECT,
                               flags=cv2.INTER_LINEAR)
@@ -61,6 +63,38 @@ def get_random_crop_img(sample: dict, rnd_angles: tuple, rnd_scales: tuple, crop
         'msk': msk_crop
     }
     return ret
+
+
+
+def _load_batch_sample(path_sample: str) -> dict:
+    with open(path_sample, 'rb') as f:
+        ret = pkl.load(f)
+    return ret
+
+
+class CountDataset_Validation(Dataset):
+
+    def __init__(self, dir_batches: str, in_memory=True):
+        self.dir_batches = dir_batches
+        self.in_memory = in_memory
+
+    def build(self):
+        self.path_batches = sorted(glob.glob(os.path.join(self.dir_batches, 'batch_*.pkl')))
+        if self.in_memory:
+            self.data = [_load_batch_sample(x) for x in self.path_batches]
+        else:
+            self.data = None
+        return self
+
+    def __len__(self):
+        return len(self.path_batches)
+
+    def __getitem__(self, item):
+        if self.in_memory:
+            sample = self.data[item]
+        else:
+            sample = _load_batch_sample(self.path_batches[item])
+        return sample
 
 
 class CountDataset(Dataset):
@@ -108,7 +142,7 @@ class CountDataset(Dataset):
         return self
 
     def __len__(self):
-        if self.dataset_size is not None:
+        if self.dataset_size is None:
             return len(self.data_idx)
         else:
             return self.dataset_size
@@ -121,7 +155,7 @@ class CountDataset(Dataset):
         return crop
 
     def __getitem__(self, item):
-        idx = np.random.randint(0, len(self))
+        idx = np.random.randint(0, len(self.data_idx))
         sample = self.get_sample(idx)
         ret = self._crop_random(sample)
         return ret
@@ -134,24 +168,53 @@ def worker_init_fn_random(idx):
     np.random.seed(seed_)
 
 
-def main_debug_dataset():
-    logging.basicConfig(level=logging.INFO)
-    path_idx = '/home/ar/data/yshad-2019/corn_seed_counting_train_v2_clean/idx-val.txt'
+def generate_validation_set(path_idx: str, num_batches=200):
+    dir_batches = path_idx + '_batches'
+    os.makedirs(dir_batches, exist_ok=True)
+    path_batch_tmpl = os.path.join(dir_batches, 'batch_{:08d}.pkl')
+    num_batches_start = len(glob.glob(os.path.join(dir_batches, 'batch_*.pkl')))
+    num_batches_end = num_batches_start + num_batches
+    #
     dataset = CountDataset(path_idx=path_idx,
                            in_memory=False,
                            dataset_size=10000000000).build()
-    dataloader = DataLoader(dataset, batch_size=16, num_workers=8, worker_init_fn=worker_init_fn_random)
-    #
-    # for xi, x in enumerate(dataset):
-    for xi, x in enumerate(dataloader):
-        # logging.info('[{}] -> {}'.format(xi, x['idx']))
-        # plt.subplot(1, 2, 1)
-        # plt.imshow(x['img'])
-        # plt.subplot(1, 2, 2)
-        # plt.imshow(x['msk'])
-        # plt.show()
+    dataloader = DataLoader(dataset, batch_size=1, num_workers=8, worker_init_fn=worker_init_fn_random)
+    dataloader_iter = iter(dataloader)
+    for xi in range(num_batches_start, num_batches_end):
+        x = next(dataloader_iter)
+        path_batch = path_batch_tmpl.format(xi)
+        batch = {x: y.cpu().numpy() for x, y in x.items()}
+        with open(path_batch, 'wb') as f:
+            pkl.dump(batch, f)
+        if (xi % 20) == 0:
+            logging.info('[{}/{}] <- {}'.format(xi, num_batches_end, path_batch))
 
-        print('-')
+
+def main_debug_dataset():
+    logging.basicConfig(level=logging.INFO)
+    path_idx = '/home/ar/data/yshad-2019/corn_seed_counting_train_v2_clean/idx-val.txt'
+    dir_batches = path_idx + '_batches'
+
+    generate_validation_set(path_idx)
+
+    # dir_batches = path_idx + '_batches'
+    # os.makedirs(dir_batches, exist_ok=True)
+    # #
+    # dataset = CountDataset(path_idx=path_idx,
+    #                        in_memory=False,
+    #                        dataset_size=10000000000).build()
+    # dataloader = DataLoader(dataset, batch_size=1, num_workers=8, worker_init_fn=worker_init_fn_random)
+    # #
+    # for xi, x in enumerate(dataset):
+    # # for xi, x in enumerate(dataloader):
+    #     # logging.info('[{}] -> {}'.format(xi, x['idx']))
+    #     # plt.subplot(1, 2, 1)
+    #     # plt.imshow(x['img'])
+    #     # plt.subplot(1, 2, 2)
+    #     # plt.imshow(x['msk'])
+    #     # plt.show()
+    #
+    #     print('-')
 
 
 if __name__ == '__main__':
